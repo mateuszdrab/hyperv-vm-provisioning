ab04ed71bb96a25d68b9d036074ec4f7e5e071ec
Mateusz Drab <mateuszd@mpd.pw>
2025-01-30 02:53:34 +0000

    feat: add scripts to copy GPU drivers and install NVIDIA Container Toolkit on Ubuntu VM

---
+++ Copy-HostGPUDriverToUbuntu.ps1
@@ -0,0 +1,30 @@
+﻿param(
+    [string]$UserName = "root",
+    [string]$VMName = $null
+)
+
+if ($VMName -eq $null) {
+    Write-Error "Please provide a VM name"
+    exit
+}
+
+# Copy the GPU driver files to the VM
+$TargetHost = (Get-VM -Name $VMName).NetworkAdapters.IPAddresses[0]
+
+# Create a destination folder.
+ssh ${UserName}@${TargetHost} "mkdir -p ~/wsl/drivers ~/wsl/lib"
+
+# Copy driver files
+# https://github.com/brokeDude2901/dxgkrnl_ubuntu/blob/main/README.md#3-copy-windows-host-gpu-driver-to-ubuntu-vm
+
+(Get-CimInstance -ClassName Win32_VideoController -Property *).InstalledDisplayDrivers | Select-String "C:\\Windows\\System32\\DriverStore\\FileRepository\\[a-zA-Z0-9\\._]+\\" | foreach {
+    $l = $_.Matches.Value.Substring(0, $_.Matches.Value.Length - 1)
+    scp -r $l ${UserName}@${TargetHost}:~/wsl/drivers/
+}
+
+scp -r C:\Windows\System32\lxss\lib ${UserName}@${TargetHost}:~/wsl/
+scp -r "C:\Program Files\WSL\lib" ${UserName}@${TargetHost}:~/wsl/
+
+scp .\install-gpu.sh ${UserName}@${TargetHost}:/tmp/
+
+ssh ${UserName}@${TargetHost} 'chmod +x /tmp/install-gpu.sh && /tmp/install-gpu.sh'
---
+++ install-gpu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+echo "Compiling dxgkrnl-dkms..."
+curl -fsSL https://content.staralt.dev/dxgkrnl-dkms/main/install.sh | sudo bash -es
+
+echo "Moving files and setting permissions..."
+mv ~/wsl /usr/lib/wsl
+chmod -R 555 /usr/lib/wsl/drivers/
+chmod -R 755 /usr/lib/wsl/lib/
+chown -R root:root /usr/lib/wsl
+sed -i '/^PATH=/ {/usr\/lib\/wsl\/lib/! s|"$|:/usr/lib/wsl/lib"|}' /etc/environment
+ln -s /usr/lib/wsl/lib/libd3d12core.so /usr/lib/wsl/lib/libD3D12Core.so
+ln -s /usr/lib/wsl/lib/libnvoptix.so.1 /usr/lib/wsl/lib/libnvoptix_loader.so.1
+
+echo "Updating ldconfig..."
+sh -c 'echo "/usr/lib/wsl/lib" > /etc/ld.so.conf.d/ld.wsl.conf'
+ldconfig
+
+echo "Installing Docker..."
+curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+sh /tmp/get-docker.sh
+
+echo "Installing NVIDIA Container Toolkit..."
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg &&
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list |
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |
+        tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+apt-get update
+apt-get install -y nvidia-container-toolkit
+
+echo "Configuring Docker for NVIDIA Container Toolkit..."
+nvidia-ctk runtime configure --runtime=docker
+
+echo "Rebooting..."
+reboot
